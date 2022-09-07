b6918aedf6a92d4bc133efb951da631ceafcfdc1
Gordon Smith <GordonJSmith@gmail.com>
2022-09-07 12:42:49 +0100

    fix:  Add missing file
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ src/telemetry/index.ts
@@ -0,0 +1,27 @@
+import * as vscode from "vscode";
+import TelemetryReporter from "@vscode/extension-telemetry";
+
+class MyTelemetryReporter extends TelemetryReporter {
+
+    dispose(): Promise<any> {
+        reporter.sendTelemetryEvent("MyTelemetryReporter.dispose");
+        return super.dispose();
+    }
+}
+
+// telemetry reporter
+export let reporter: TelemetryReporter;
+
+export function activate(context: vscode.ExtensionContext) {
+    const extPackageJSON = context.extension.packageJSON;
+    reporter = new MyTelemetryReporter("GodonSmith.ojs", extPackageJSON.version, "0bd5634a-3457-44c2-aaf3-43b03a68eb45");
+    context.subscriptions.push(reporter);
+
+    reporter.sendTelemetryEvent("activate");
+}
+
+export function deactivate(): void {
+    reporter.sendTelemetryEvent("deactivate");
+
+    reporter.dispose();
+}
