4b4acac2f2e02269dfe3527a8d3332f6d813c94f
Gordon Smith <GordonJSmith@gmail.com>
2022-10-23 17:05:59 +0100

    WIP

--- src/notebook/controller/command.ts
+++ src/notebook/controller/command.ts
@@ -11,31 +11,10 @@ export class Commands {
         this._ctx = ctx;
 
         ctx.subscriptions.push(vscode.commands.registerCommand("ojs.download", this.download, this));
+        ctx.subscriptions.push(vscode.commands.registerCommand("ojs.cell.name", this.cellName, this));
+        ctx.subscriptions.push(vscode.commands.registerCommand("ojs.cell.db", this.dbName, this));
 
-        ctx.subscriptions.push(vscode.commands.registerCommand("ojs.cell.name", async (cell: vscode.NotebookCell) => {
-            const node = serializer.node(cell);
-            node.name = node.name ?? "";
-            const name = await vscode.window.showInputBox({ value: node.name, title: "Result Name", placeHolder: "Result Name" });
-            if (name) {
-                node.name = name;
-            }
-        }));
-
-        ctx.subscriptions.push(vscode.commands.registerCommand("ojs.cell.db", async (cell: vscode.NotebookCell) => {
-            const node = serializer.node(cell);
-            const data = {
-                ...node?.data
-            };
-            data.source = {
-                name: "",
-                ...node?.data?.source,
-            };
-            const name = await vscode.window.showInputBox({ value: node.data.source.name, title: "Database Name", placeHolder: "Database Name" });
-            if (name) {
-                node.data.source.name = name;
-            }
-        }));
-
+        // cell toolbar meta  ---
         ctx.subscriptions.push(vscode.window.onDidChangeNotebookEditorSelection(e => {
             const cell = e.notebookEditor.notebook.cellAt(e.selections[0]?.start);
             vscode.commands.executeCommand("setContext", "cellLangId", cell.document.languageId);
@@ -43,12 +22,10 @@ export class Commands {
 
         // status bar provider
         ctx.subscriptions.push(vscode.notebooks.registerNotebookCellStatusBarItemProvider("ecl-notebook", new class implements vscode.NotebookCellStatusBarItemProvider {
-
             provideCellStatusBarItems(cell: vscode.NotebookCell) {
                 return [];
             }
         }));
-
     }
 
     static attach(ctx: vscode.ExtensionContext): Commands {
@@ -86,4 +63,28 @@ export class Commands {
         }
     }
 
+    async cellName(cell: vscode.NotebookCell) {
+        const node = serializer.node(cell);
+        node.name = node.name ?? "";
+        const name = await vscode.window.showInputBox({ value: node.name, title: "Result Name", placeHolder: "Result Name" });
+        if (name) {
+            node.name = name;
+        }
+    }
+
+    async dbName(cell: vscode.NotebookCell) {
+        const node = serializer.node(cell);
+        const data = {
+            ...node?.data
+        };
+        data.source = {
+            name: "",
+            ...node?.data?.source,
+        };
+        const name = await vscode.window.showInputBox({ value: node.data.source.name, title: "Database Name", placeHolder: "Database Name" });
+        if (name) {
+            node.data.source.name = name;
+        }
+    }
 }
+
